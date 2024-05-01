ecc0d3cafd8efc605c6d24c870e14f108d50c086
Jorge Amat Soler <amjsoler@gmail.com>
2024-05-01 09:49:29 +0200

    PProject preparation. Created TODO list  at index.js. Created package.json with a dev script

---
+++ server/index.js
@@ -0,0 +1,8 @@
+/*
+//TODO:
+- Inicializar el servidor en X puerto
+- Manejador de conexiones entrantes al chat
+- manejador para cuando entra un nuevo mensaje al chat
+- manejador para cuando alguien escribe una letra
+- manejador para cuando alguien se desconecta del chat
+ */
